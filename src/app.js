353405d89c2cc1beb61c42f1162557cbd3369380
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-10-27 16:50:14 -0300

    Some routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid} = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -12,18 +13,54 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const {title, url, techs} = request.body
+
+  const newRepo = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs,
+    likes: 0 
+      }
+
+      repositories.push(newRepo)
+
+      return response.json(newRepo)
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const {id} = request.params
+  const {title, url, techs} = request.body
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id == id)
+
+  const newRepo = { 
+    id, 
+    title, 
+    url, 
+    techs
+      }
+
+      repositories[repositoryIndex] = newRepo
+
+      return response.json(newRepo)
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id == id)
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
