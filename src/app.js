7beaec9ea3e5b20cfb0f0f01c7d07e1279f4a3b6
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2020-10-27 20:00:22 -0300

    challenge all routes concluded

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   // TODO
   const {title, url, techs} = request.body
-
+ 
   const newRepo = { 
     id: uuid(), 
     title, 
@@ -28,7 +28,6 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0 
       }
-
       repositories.push(newRepo)
 
       return response.json(newRepo)
@@ -40,16 +39,20 @@ app.put("/repositories/:id", (request, response) => {
   const {title, url, techs} = request.body
 
   const repositoryIndex = repositories.findIndex(repo => repo.id == id)
+  
+  if(repositoryIndex < 0){
+    return response.status(400).send({error:"NOT POSSIBLE TO UPDATE REPOSITORY - PLEASE CHECK YOUR CODE"})
+  }
 
   const newRepo = { 
     id, 
     title, 
     url, 
-    techs
+    techs,
+    likes: repositories[repositoryIndex].likes
       }
 
       repositories[repositoryIndex] = newRepo
-
       return response.json(newRepo)
 });
 
@@ -58,6 +61,10 @@ app.delete("/repositories/:id", (request, response) => {
   const {id} = request.params
   const repositoryIndex = repositories.findIndex(repo => repo.id == id)
 
+  if(repositoryIndex < 0){
+    return response.status(400).send({error:"NOT POSSIBLE TO DELETE REPOSITORY - PLEASE CHECK YOUR CODE"})
+  }
+
   repositories.splice(repositoryIndex, 1)
 
   return response.status(204).send()
@@ -65,6 +72,18 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id == id)
+
+  if(repositoryIndex < 0){
+    return response.status(400).send({error:"NOT POSSIBLE TO GIVE LIKES TO YOUR REPOSITORY - PLEASE CHECK YOUR CODE"})
+  }
+
+  repositories[repositoryIndex].likes++
+  console.log(repositories[repositoryIndex].likes)
+
+  return response.json(repositories[repositoryIndex])
+
 });
 
 module.exports = app;
